f91989bf7695829ee47764e3ec25a0c7eef50d21
Greg McClarnon <greg@splaatphoto.co.uk>
2024-02-26 15:54:31 +0000

    Feat: add score reporting and display win at 5 points

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+
+let playerScore = 0;
+let computerScore = 0;
+
+let resultsDiv = document.getElementById('results');
+
+function displayScore() {
+
+    resultsDiv.innerHTML += `<p>Player score: ${playerScore}</br>Computer score: ${computerScore}</p>`;
+}
+
+function isThereAWinner() {
+    
+    if (playerScore === 5) {
+        resultsDiv.innerHTML += "<p>Well done! You have won 5 points! You win the game.</p>";
+        playerScore = 0;
+        computerScore = 0;
+    }
+
+    if (computerScore === 5) {
+        resultsDiv.innerHTML += "<p>Bad luck! The computer has won 5 points! They win the game.</p>";
+        playerScore = 0;
+        computerScore = 0;
+    }
+    
+}
+
 function playRound(playerSelection) {
     
     // User decides
@@ -27,38 +54,62 @@ function playRound(playerSelection) {
 
     // Calculate the result
 
-    // Result overide
 
     if (playerSelection==computerSelection) {
-        console.log("Draw. Play again");
-        playRound();
+        resultsDiv.innerHTML += "<p>Draw. Play again.</p>";
+        console.log("Draw. Play again.")
+        displayScore();
     }
 
     else {
         if (playerSelection=="rock") {
             if (computerSelection=="scissors"){
                 console.log("You win!");
+                resultsDiv.innerHTML += '<p>You win!</p>';
+                playerScore++;
+                displayScore();
+                isThereAWinner();
             }
             else {
                 console.log("Computer wins!");
+                resultsDiv.innerHTML += '<p>Computer wins!</p>';
+                computerScore++;
+                displayScore();
+                isThereAWinner();
             }
         }
 
         if (playerSelection=="paper") {
             if (computerSelection=="rock"){
+                resultsDiv.innerHTML += '<p>You win!</p>';
                 console.log("You win!");
+                playerScore++;
+                displayScore();
+                isThereAWinner();
             }
             else {
                 console.log("Computer wins!");
+                resultsDiv.innerHTML += '<p>Computer wins!</p>';
+                computerScore++;
+                displayScore();
+                isThereAWinner();
             }
         }
 
         if (playerSelection=="scissors") {
             if (computerSelection=="paper"){
                 console.log("You win!");
+                resultsDiv.innerHTML += '<p>You win!</p>';
+                playerScore++;
+                displayScore();
+                isThereAWinner();
             }
             else {
                 console.log("Computer wins!");
+                resultsDiv.innerHTML += '<p>Computer wins!</p>';
+                computerScore++;
+                displayScore();
+                isThereAWinner();
             }
         }
     }
